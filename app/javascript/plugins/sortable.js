7fb06c9da3ac94394b0600c35fcfcf749f8ef5cb
robin trouillet <robintrouillet@hotmail.fr>
2021-11-28 20:49:17 +0100

    dim soir

--- app/javascript/plugins/sortable.js
+++ app/javascript/plugins/sortable.js
@@ -4,7 +4,11 @@ import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
 const initSortable = () => {
   if (document.querySelector('#sortable-items')) {
     var el = document.getElementById('sortable-items');
-    var sortable = Sortable.create(el);
+    var sortable = Sortable.create(el, {
+      sort: true,
+      animation: 150,
+      dataIdAttr: 'data-id',
+    });
   }
 }
 
