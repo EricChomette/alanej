42746db1a3cab2ac8162060ebb354623fb3f512d
robin trouillet <robintrouillet@hotmail.fr>
2021-11-28 19:31:59 +0100

    form index

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,7 +4,8 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 const initFlatpickr = () => {
   const datesForm = document.getElementById('home-logo');
-  if (datesForm) {
+
+  if (datesForm || indexDatesForm) {
     flatpickr("#range_start", {
       plugins: [new rangePlugin({ input: "#range_end" })],
       minDate: "today",
@@ -12,6 +13,18 @@ const initFlatpickr = () => {
       dateFormat: "Y-m-d",
     })
   }
+
+  const indexDatesForm = document.getElementById('index_range_start');
+  if (indexDatesForm) {
+    flatpickr("#index_range_start", {
+      plugins: [new rangePlugin({ input: "#index_range_end" })],
+      minDate: "today",
+      inline: false,
+      dateFormat: "Y-m-d",
+    })
+  }
+
+
 };
 
 export { initFlatpickr };
