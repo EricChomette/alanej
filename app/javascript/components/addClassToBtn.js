3560fa163507cfa703754583720dfa2d75e4d272
Mael Piro <piromael46@gmail.com>
2021-11-29 10:32:26 +0100

    chargeoui

--- app/javascript/components/addClassToBtn.js
+++ app/javascript/components/addClassToBtn.js
@@ -1,13 +1,29 @@
 const loading= () =>{
- if (document.querySelector("#goAnim")) {
-  const div = document.querySelector(".home-container");
-  const div2 = document.querySelector(".load-container");
-  const btn = document.querySelector("#goAnim");
-  
-    btn.addEventListener("click", function(){
-      div.classList.add("elementToFadeIn");
-      div2.classList.add("elementToFadeOut"); 
-        
+  if (document.querySelector("#goAnim")) {
+    const div = document.querySelector(".home-container");
+    const div2 = document.querySelector(".load-container");
+    const btn = document.querySelector("#goAnim");
+    const txt = document.querySelector(".load-text");
+      btn.addEventListener("click", function(){
+        div.classList.add("elementToFadeIn");
+        div2.classList.add("elementToFadeOut");
+        txt.classList.add("clignote");
+        var titles = [
+         "Recherche itinéraire...",
+         "Recupération des données méteo",
+         "Recherche de puff...",
+         "Selection des stations..."
+      ];
+function newTitle () {
+    var i = (Math.random() * titles.length) | 0;
+    txt.innerText = titles[i];
+}
+
+newTitle();   
+      setInterval(function(){ 
+        //code goes here that will be run every 5 seconds.
+        newTitle(); 
+    }, 4000);
     });
     
   }
